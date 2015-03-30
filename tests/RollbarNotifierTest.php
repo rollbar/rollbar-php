61b6fef9e39a65327a69f70f96eb373435c43538
Brian Rue <brian@rollbar.com>
2015-03-30 16:14:45 -0700

    More tests; disable travis for 5.2

--- tests/RollbarNotifierTest.php
+++ tests/RollbarNotifierTest.php
@@ -1,12 +1,16 @@
 <?php
 
-define('ROLLBAR_TEST_TOKEN', 'ad865e76e7fb496fab096ac07b1dbabb');
+if (!defined('ROLLBAR_TEST_TOKEN')) {
+    define('ROLLBAR_TEST_TOKEN', 'ad865e76e7fb496fab096ac07b1dbabb');
+}
 
 class RollbarNotifierTest extends PHPUnit_Framework_TestCase {
     
     private static $simpleConfig = array(
         'access_token' => ROLLBAR_TEST_TOKEN,
-        'environment' => 'test'
+        'environment' => 'test',
+        'root' => '/path/to/code/root',
+        'code_version' => RollbarNotifier::VERSION
     );
 
     public function testConstruct() {
@@ -76,6 +80,67 @@ class RollbarNotifierTest extends PHPUnit_Framework_TestCase {
         // TODO assert that the payload actually contains the person
         
         $this->assertStringMatchesFormat('%x-%x-%x-%x-%x', $uuid);
+    }
+
+    public function testExceptionWithInvalidConfig() {
+        $config = self::$simpleConfig;
+        $config['access_token'] = 'hello';
+        $notifier = new RollbarNotifier($config);
+
+        $result = 'dummy';
+        try {
+            throw new Exception("test exception");
+        } catch (Exception $e) {
+            $result = $notifier->report_exception($e);
+        }
+
+        $this->assertNull($result);
+    }
+
+    public function testErrorWithoutCaptureBacktrace() {
+        $config = self::$simpleConfig;
+        $config['capture_error_backtraces'] = false;
+        $notifier = new RollbarNotifier($config);
+
+        $uuid = $notifier->report_php_error(E_WARNING, 'Some warning', 'the_file.php', 2);
+        
+        $this->assertStringMatchesFormat('%x-%x-%x-%x-%x', $uuid);
+    }
+
+    public function testAgentBatched() {
+        $config = self::$simpleConfig;
+        $config['handler'] = 'agent';
+        $notifier = new RollbarNotifier($config);
+
+        $uuid = $notifier->report_message('Hello world');
+        $this->assertStringMatchesFormat('%x-%x-%x-%x-%x', $uuid);
+
+        $notifier->flush();
+
+        $this->assertEquals(0, $notifier->queueSize());
+    }
+    
+    public function testAgentNonbatched() {
+        $config = self::$simpleConfig;
+        $config['handler'] = 'agent';
+        $config['batched'] = false;
+        $notifier = new RollbarNotifier($config);
+
+        $uuid = $notifier->report_message('Hello world');
+        $this->assertStringMatchesFormat('%x-%x-%x-%x-%x', $uuid);
+
+        $this->assertEquals(0, $notifier->queueSize());
+    }
+    
+    public function testBlockingNonbatched() {
+        $config = self::$simpleConfig;
+        $config['batched'] = false;
+        $notifier = new RollbarNotifier($config);
+
+        $uuid = $notifier->report_message('Hello world');
+        $this->assertStringMatchesFormat('%x-%x-%x-%x-%x', $uuid);
+
+        $this->assertEquals(0, $notifier->queueSize());
     }
 
 }
