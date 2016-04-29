b8a1c6523964513b7b5127226c651ba7805bf012
Christopher Pfohl <Christopher.Pfohl@gmail.com>
2016-04-29 11:39:31 -0400

    Finish Payload tests

--- tests/PayloadTest.php
+++ tests/PayloadTest.php
@@ -11,7 +11,7 @@ class PayloadTest extends \PHPUnit_Framework_TestCase {
         $payload = new Payload();
     }
 
-    public function testPayloadConstructorAcceptsBody() {
+    public function testPayloadBody() {
         $bodyContent = m::mock("Rollbar\Payload\ContentInterface");
         $body = new Body($bodyContent);
         $payload = new Payload($body);
@@ -19,11 +19,30 @@ class PayloadTest extends \PHPUnit_Framework_TestCase {
         $this->assertEquals($body, $payload->getBody());
     }
 
-    public function testPayloadConstructorAcceptsAccessToken() {
+    public function testPayloadAccessToken() {
         $bodyContent = m::mock("Rollbar\Payload\ContentInterface");
-        $accessToken = "abcdef0123456789abcdef0123456789";
-        $payload = new Payload(new Body($bodyContent), $accessToken);
+        $body = new Body($bodyContent);
 
+        $accessToken = null;
+        $payload = new Payload($body, $accessToken);
+        $this->assertNull($payload->getAccessToken());
+
+        $accessToken = "too_short";
+        try {
+            new Payload($body, $accessToken);
+        } catch (\InvalidArgumentException $e) {
+            $this->assertContains("32", $e->getMessage());
+        }
+
+        $accessToken = "too_longtoo_longtoo_longtoo_longtoo_longtoo_long";
+        try {
+            new Payload($body, $accessToken);
+        } catch (\InvalidArgumentException $e) {
+            $this->assertContains("32", $e->getMessage());
+        }
+
+        $accessToken = "012345678901234567890123456789ab";
+        $payload = new Payload($body, $accessToken);
         $this->assertEquals($accessToken, $payload->getAccessToken());
     }
 }
