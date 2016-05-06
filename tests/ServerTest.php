03745bccd3e49340913292927858deb8f8a82fcb
Christopher Pfohl <Christopher.Pfohl@gmail.com>
2016-05-06 18:33:21 -0500

    Add Server

---
+++ tests/ServerTest.php
@@ -0,0 +1,79 @@
+<?php namespace Rollbar;
+
+class ServerTest
+{
+    public function testHost()
+    {
+        $val = "TEST";
+        $server = (new Server())
+            ->setHost($val);
+        $this->assertEquals($val, $server->getHost());
+
+        $val2 = "TEST2";
+        $this->assertEquals($val2, $server->setHost($val2)->getHost());
+    }
+
+    public function testRoot()
+    {
+        $val = "TEST";
+        $server = (new Server())
+            ->setRoot($val);
+        $this->assertEquals($val, $server->getRoot());
+
+        $val2 = "TEST2";
+        $this->assertEquals($val2, $server->setRoot($val2)->getRoot());
+    }
+
+    public function testBranch()
+    {
+        $val = "TEST";
+        $server = (new Server())
+            ->setBranch($val);
+        $this->assertEquals($val, $server->getBranch());
+
+        $val2 = "TEST2";
+        $this->assertEquals($val2, $server->setBranch($val2)->getBranch());
+    }
+
+    public function testCodeVersion()
+    {
+        $val = "TEST";
+        $server = (new Server())
+            ->setCodeVersion($val);
+        $this->assertEquals($val, $server->getCodeVersion());
+
+        $val2 = "TEST2";
+        $this->assertEquals($val2, $server->setCodeVersion($val2)->getCodeVersion());
+    }
+
+    public function testExtra()
+    {
+        $server = new Server();
+        $server->test = "testing";
+        $this->assertEquals("testing", $server->test);
+    }
+
+    public function testEncode()
+    {
+        $server = (new Server())
+            ->setHost("server2-ec-us")
+            ->setRoot("/home/app/testingRollbar")
+            ->setBranch("master")
+            ->setCodeVersion("#dca015");
+        $server->test = array(1, 2, "3", array());
+        $expected = '{' .
+            '"host":"server2-ec-us",' .
+            '"root":"\\/home\\/app\\/testingRollbar",' .
+            '"branch":"master",' .
+            '"code_version":"#dca015",' .
+            '"test":' .
+                '[' .
+                    '1,' .
+                    '2,' .
+                    '"3",' .
+                    '[]' .
+                ']' .
+            '}';
+        $this->assertEquals($expected, json_encode($server));
+    }
+}
