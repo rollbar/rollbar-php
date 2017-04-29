1509e5f915b791ed5520033d182243bd993a563f
Artur Moczulski <artur.moczulski@gmail.com>
2017-04-29 07:17:21 +0000

    github-17: no need to force HTTP rc 500 - default PHP exception handler causes HTTP rc 500 on display_errors=0 by default; just have to restore it after logging with Rollbar

--- src/Rollbar.php
+++ src/Rollbar.php
@@ -55,13 +55,8 @@ class Rollbar
     {
         self::log(Level::error(), $exception);
         
-        if (ini_get('display_errors')) {
-            restore_exception_handler();
-            throw $exception;
-        } else {
-            header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
-            exit(1);
-        }
+        restore_exception_handler();
+        throw $exception;
     }
 
     public static function log($level, $toLog, $extra = array())
