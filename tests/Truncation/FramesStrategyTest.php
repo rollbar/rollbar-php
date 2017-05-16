7139935bb1bd9825f29b5f65f89f04303b0e014a
Artur Moczulski <artur.moczulski@gmail.com>
2017-05-16 22:36:32 +0000

    github-38: clean up long lines

--- tests/Truncation/FramesStrategyTest.php
+++ tests/Truncation/FramesStrategyTest.php
@@ -41,26 +41,38 @@ class FramesStrategyTest extends \PHPUnit_Framework_TestCase
                 array('data' => array('body' =>
                     array(
                         'trace' => array(
-                            'frames' => range(1, FramesStrategy::FRAMES_OPTIMIZATION_RANGE * 2 + 1)
+                            'frames' => range(
+                                1,
+                                FramesStrategy::FRAMES_OPTIMIZATION_RANGE * 2 + 1
+                            )
                         )
                     )
                 )),
                 array_merge(
                     range(1, FramesStrategy::FRAMES_OPTIMIZATION_RANGE),
-                    range(FramesStrategy::FRAMES_OPTIMIZATION_RANGE + 2, FramesStrategy::FRAMES_OPTIMIZATION_RANGE * 2 + 1)
+                    range(
+                        FramesStrategy::FRAMES_OPTIMIZATION_RANGE + 2, 
+                        FramesStrategy::FRAMES_OPTIMIZATION_RANGE * 2 + 1
+                    )
                 )
             ),
             'truncate middle using trace_chain key' => array(
                 array('data' => array('body' =>
                     array(
                         'trace_chain' => array(
-                            'frames' => range(1, FramesStrategy::FRAMES_OPTIMIZATION_RANGE * 2 + 1)
+                            'frames' => range(
+                                1, 
+                                FramesStrategy::FRAMES_OPTIMIZATION_RANGE * 2 + 1
+                            )
                         )
                     )
                 )),
                 array_merge(
                     range(1, FramesStrategy::FRAMES_OPTIMIZATION_RANGE),
-                    range(FramesStrategy::FRAMES_OPTIMIZATION_RANGE + 2, FramesStrategy::FRAMES_OPTIMIZATION_RANGE * 2 + 1)
+                    range(
+                        FramesStrategy::FRAMES_OPTIMIZATION_RANGE + 2, 
+                        FramesStrategy::FRAMES_OPTIMIZATION_RANGE * 2 + 1
+                    )
                 )
             )
         );
