ccdd1f4935fd60adb397f9aa787480947dbf2efa
Artur Moczulski <artur.moczulski@gmail.com>
2017-05-29 23:39:44 +0000

    github-137: composer fix

--- src/RollbarJsHelper.php
+++ src/RollbarJsHelper.php
@@ -6,8 +6,8 @@ class RollbarJsHelper
     protected $config;
     
     /**
-     * @param array $config The array passed to he should have the same 
-     * available options that you can find in Rollbar.js, using symbols or 
+     * @param array $config The array passed to he should have the same
+     * available options that you can find in Rollbar.js, using symbols or
      * strings for the keys.
      */
     public function __construct($config)
