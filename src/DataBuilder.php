74ca64756061a1843f0773fddf2e77b9460b3dfa
Andrew Ledvina <wvvwwvw@gmail.com>
2017-06-23 08:14:16 -0700

    move uuid4 to utilities out of databuilder

--- src/DataBuilder.php
+++ src/DataBuilder.php
@@ -897,7 +897,7 @@ class DataBuilder implements DataBuilderInterface
 
     protected function getUuid()
     {
-        return self::uuid4();
+        return $this->utilities->uuid4();
     }
 
     protected function getNotifier()
@@ -910,31 +910,6 @@ class DataBuilder implements DataBuilderInterface
         return $this->baseException;
     }
 
-    // from http://www.php.net/manual/en/function.uniqid.php#94959
-    protected static function uuid4()
-    {
-        mt_srand();
-        return sprintf(
-            '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-            // 32 bits for "time_low"
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff),
-            // 16 bits for "time_mid"
-            mt_rand(0, 0xffff),
-            // 16 bits for "time_hi_and_version",
-            // four most significant bits holds version number 4
-            mt_rand(0, 0x0fff) | 0x4000,
-            // 16 bits, 8 bits for "clk_seq_hi_res",
-            // 8 bits for "clk_seq_low",
-            // two most significant bits holds zero and one for variant DCE1.1
-            mt_rand(0, 0x3fff) | 0x8000,
-            // 48 bits for "node"
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff)
-        );
-    }
-    
     /**
      * Parses an array of code lines from source file with given filename.
      *
