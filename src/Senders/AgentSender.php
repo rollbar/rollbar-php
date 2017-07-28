ae01604c03912832c592e5c02ecb16b9c0fdb306
Andrew Weiss <wvvwwvw@gmail.com>
2017-07-28 16:38:02 +0200

    all of the senders should return a response

--- src/Senders/AgentSender.php
+++ src/Senders/AgentSender.php
@@ -2,6 +2,7 @@
 
 namespace Rollbar\Senders;
 
+use Rollbar\Response;
 use Rollbar\Payload\Payload;
 
 class AgentSender implements SenderInterface
@@ -28,6 +29,9 @@ class AgentSender implements SenderInterface
             $this->loadAgentFile();
         }
         fwrite($this->agentLog, json_encode($scrubbedPayload) . "\n");
+
+        $uuid = $scrubbedPayload['data']['uuid'];
+        return new Response(0, "Written to agent file", $uuid);
     }
 
     /**
