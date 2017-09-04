1a514da879571fd6490c486fbf8d93b4695baddd
Artur Moczulski <artur.moczulski@gmail.com>
2017-09-04 08:31:53 +0000

    github-259: remove shiftFunction configuration

--- src/DataBuilder.php
+++ src/DataBuilder.php
@@ -45,7 +45,6 @@ class DataBuilder implements DataBuilderInterface
     protected $baseException;
     protected $includeCodeContext;
     protected $includeExcCodeContext;
-    protected $shiftFunction;
     protected $sendMessageTrace;
     protected $rawRequestBody;
     protected $localVarsDump;
@@ -99,11 +98,6 @@ class DataBuilder implements DataBuilderInterface
         $this->setLocalVarsDump($config);
         $this->setCaptureErrorStacktraces($config);
         $this->setLevelFactory($config);
-
-        $this->shiftFunction = $this->tryGet($config, 'shift_function');
-        if (!isset($this->shiftFunction)) {
-            $this->shiftFunction = true;
-        }
     }
 
     protected function tryGet($array, $key)
@@ -462,13 +456,6 @@ class DataBuilder implements DataBuilderInterface
             }
 
             $frames[] = $frame;
-        }
-
-        if ($this->shiftFunction && count($frames) > 0) {
-            for ($i = count($frames) - 1; $i > 0; $i--) {
-                $frames[$i]->setMethod($frames[$i - 1]->getMethod());
-            }
-            $frames[0]->setMethod('<main>');
         }
         
         $frames = array_reverse($frames);
