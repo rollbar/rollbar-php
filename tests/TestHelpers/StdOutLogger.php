46f1990d39fe22a7a0c2fbb7a643574a5af9aae6
Artur Moczulski <artur.moczulski@gmail.com>
2018-01-26 19:46:32 +0000

    github-292: PHP 5.3 doesn't support traits

--- tests/TestHelpers/StdOutLogger.php
+++ tests/TestHelpers/StdOutLogger.php
@@ -7,8 +7,6 @@ use Psr\Log\LoggerTrait;
 
 class StdOutLogger extends RollbarLogger
 {
-    use LoggerTrait;
-    
     public function log($level, $toLog, array $context = array(), $isUncaught = false)
     {
         echo '[' . get_class($this) . ': '. $level . '] ' . $message;
