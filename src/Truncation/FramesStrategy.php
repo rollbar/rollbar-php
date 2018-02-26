1a22a6e05a835565d9776b30ca43fbab20dc5e37
Warren Strong <w@uber.co.nz>
2018-02-26 17:38:05 +1300

    Update style

--- src/Truncation/FramesStrategy.php
+++ src/Truncation/FramesStrategy.php
@@ -2,32 +2,32 @@
 
 class FramesStrategy extends AbstractStrategy
 {
-    
+
     const FRAMES_OPTIMIZATION_RANGE = 75;
-    
+
     public function execute(array $payload)
     {
-		$trace_or_chain = false;
+        $key = false;
 
         if (isset($payload['data']['body']['trace_chain']['frames'])) {
-			$trace_or_chain = 'trace_chain';
+            $key = 'trace_chain';
         } elseif (isset($payload['data']['body']['trace']['frames'])) {
-			$trace_or_chain = 'trace';
+            $key = 'trace';
         }
 
-		if ($trace_or_chain) {
-			$payload['data']['body'][$trace_or_chain]['frames'] = $this->selectFrames($payload['data']['body'][$trace_or_chain]['frames']);
-		}
+        if ($key) {
+            $payload['data']['body'][$key]['frames'] = $this->selectFrames($payload['data']['body'][$key]['frames']);
+        }
 
-		return $payload;
+        return $payload;
     }
-    
+
     public function selectFrames($frames, $range = self::FRAMES_OPTIMIZATION_RANGE)
     {
         if (count($frames) <= $range * 2) {
             return $frames;
         }
-        
+
         return array_merge(
             array_splice($frames, 0, $range),
             array_splice($frames, -$range, $range)
