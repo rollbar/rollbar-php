e1914601fb717694e5f5ffc9078c9d6264eee171
Artur Moczulski <artur.moczulski@gmail.com>
2018-08-06 01:35:17 +0000

    385: add type information about non-serializable objects

--- src/Utilities.php
+++ src/Utilities.php
@@ -81,7 +81,13 @@ final class Utilities
                 $val = $val->serialize();
             } elseif (is_array($val)) {
                 $val = self::serializeForRollbar($val);
+            } elseif (is_object($val)) {
+                $val = array(
+                    'class' => get_class($val),
+                    'value' => $val
+                );
             }
+            
             if ($customKeys !== null && in_array($key, $customKeys)) {
                 $returnVal[$key] = $val;
             } elseif (!is_null($val)) {
