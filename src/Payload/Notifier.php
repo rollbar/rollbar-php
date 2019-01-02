ee4d050222e638ab0133f053799a450dedf7e7a0
Artur Moczulski <artur.moczulski@gmail.com>
2019-01-02 14:23:53 +0000

    Bump version number to v1.7.3

--- src/Payload/Notifier.php
+++ src/Payload/Notifier.php
@@ -3,7 +3,7 @@
 class Notifier implements \Serializable
 {
     const NAME = "rollbar-php";
-    const VERSION = "1.7.1";
+    const VERSION = "1.7.3";
 
     public static function defaultNotifier()
     {
