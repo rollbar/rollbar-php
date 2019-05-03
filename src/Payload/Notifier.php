889501d445e6096c037f7098e2a7121657290de8
Artur Moczulski <artur.moczulski@gmail.com>
2019-05-04 01:11:05 +0200

    chore(version): release v1.8.0

--- src/Payload/Notifier.php
+++ src/Payload/Notifier.php
@@ -3,7 +3,7 @@
 class Notifier implements \Serializable
 {
     const NAME = "rollbar-php";
-    const VERSION = "1.7.5";
+    const VERSION = "1.8.0";
 
     public static function defaultNotifier()
     {
