04ea032d175854e2397a58114db33cbcb75337c8
Sachin Goyal <sachin@rollbar.com>
2020-05-04 17:26:34 -0700

    [X] Using test case new path.

--- tests/BaseRollbarTest.php
+++ tests/BaseRollbarTest.php
@@ -1,7 +1,10 @@
 <?php namespace Rollbar;
 
-abstract class BaseRollbarTest extends \PHPUnit\Framework\TestCase
+use PHPUnit\Framework\TestCase;
+
+abstract class BaseRollbarTest extends TestCase
 {
+
     const DEFAULT_ACCESS_TOKEN = 'ad865e76e7fb496fab096ac07b1dbabb';
     
     public function tearDown(): void
