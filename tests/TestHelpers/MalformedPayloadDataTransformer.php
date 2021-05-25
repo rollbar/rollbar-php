21bcb4d1b32282661e5af68404fff13489d30875
Bishop Bettini <charles.bettini@rollbar.com>
2021-05-25 12:42:50 -0400

    feat: strong type Config parameters
    A few tests were playing fast and loose with weak-typed Config
    values, so also correct these.

--- tests/TestHelpers/MalformedPayloadDataTransformer.php
+++ tests/TestHelpers/MalformedPayloadDataTransformer.php
@@ -1,11 +1,16 @@
 <?php namespace Rollbar\TestHelpers;
 
+use \Rollbar\Payload\Level;
 use \Rollbar\Payload\Payload;
 
 class MalformedPayloadDataTransformer implements \Rollbar\TransformerInterface
 {
-    public function transform(Payload $payload, $level, $toLog, $context)
-    {
+    public function transform(
+        Payload $payload,
+        Level|string $level,
+        mixed $toLog,
+        array $context = array()
+    ): ?Payload {
         $mock = \Mockery::mock('\Rollbar\Payload\Data')->makePartial();
         $mock->shouldReceive("serialize")->andReturn(false);
         $levelFactory = new \Rollbar\LevelFactory();
