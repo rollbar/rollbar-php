484daa9f1bf1fac15b52122268d9127b6435fdd1
Daniel Morell <daniel@cairntek.com>
2022-10-24 16:18:39 -0500

    Added support for psr/log v3

--- tests/TestHelpers/StdOutLogger.php
+++ tests/TestHelpers/StdOutLogger.php
@@ -2,13 +2,14 @@
 
 namespace Rollbar\TestHelpers;
 
+use Stringable;
 use Rollbar\RollbarLogger;
 use Psr\Log\LoggerTrait;
 
 class StdOutLogger extends RollbarLogger
 {
-    public function log($level, $toLog, array $context = array())
+    public function log($level, string|Stringable $message, array $context = array()): void
     {
-        echo '[' . get_class($this) . ': '. $level . '] ' . $message;
+        echo '[' . get_class($this) . ': ' . $level . '] ' . $message;
     }
 }
