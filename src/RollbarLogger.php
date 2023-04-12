735d1f31e53b9fd6424b405cc9479c57ecaa8e00
Daniel Morell <dan.c.morell@gmail.com>
2023-04-12 06:39:36 -0500

    Backport of #614 Added response status code to verbose log message.

--- src/RollbarLogger.php
+++ src/RollbarLogger.php
@@ -142,7 +142,8 @@ class RollbarLogger extends AbstractLogger
         } elseif ($response->getStatus() >= 400) {
             $info = $response->getInfo();
             $this->verboseLogger()->error(
-                'Occurrence rejected by the API: ' . ($info['message'] ?? 'message not set')
+                'Occurrence rejected by the API: with status ' . $response->getStatus() . ': '
+                . ($info['message'] ?? 'message not set')
             );
         } else {
             $this->verboseLogger()->info('Occurrence successfully logged');
